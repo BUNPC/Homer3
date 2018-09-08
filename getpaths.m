45e179b80d733864106b2e247b7ea829c93813cf
jdubb <44243610+jayd1860@users.noreply.github.com>
2018-09-08 05:21:18 +0000

    v1.2  -- Change setpaths again to not automatically remove all directories of conflicting workspaces but rather to change the order of precedence to make the current workspace the primary one in the search paths and report on all other workspaces that have conflicting functions. Report also on the order of precedence.

--- getpaths.m
+++ getpaths.m
@@ -1,4 +1,11 @@
-function paths = getpaths()
+function [paths, wspaths, paths_excl_str] = getpaths(mode)
+
+DEBUG = 0;
+
+if ~exist('mode','var') || isempty(mode)
+    mode = 'errcheck';
+end
+
 
 paths = {...
     '/'; ...
@@ -26,22 +33,62 @@ paths = {...
     '/stimGUI'; ...
     };
 
-% Remove excluded paths from other applications
-while 1
-    paths_excl = exclpaths({'Homer3.m','Homer2_UI.m'}, './utils');
-    if isempty(paths_excl)
-        break;
+
+wspaths = {};
+paths_excl_str = {};
+
+if isempty(strfind(mode, 'noerrcheck'))  && ~isempty(strfind(mode, 'errcheck'))
+        
+    % Get all workspace paths that have similar functions sets with current applications
+    appmainfunc = {'Homer3.m','Homer2_UI.m'};
+    
+    kk=1;
+    wsidx = [];
+    for ii=1:length(appmainfunc)
+        
+        while 1
+            
+            [paths_excl, foo] = getactivewspace(appmainfunc{ii});
+            if isempty(paths_excl)
+                break;
+            end
+            
+            wspaths{kk,1} = foo;
+            if pathscompare(wspaths{kk}, pwd)
+                wsidx = kk;
+            end
+            
+            paths_excl_str{kk} = '';
+            for jj=1:length(paths_excl)
+                if DEBUG
+                    fprintf('Removing path %s\n', paths_excl{jj});
+                end
+                if isempty(paths_excl_str{kk})
+                    paths_excl_str{kk} = paths_excl{jj};
+                else
+                    paths_excl_str{kk} = [paths_excl_str{kk}, delimiter, paths_excl{jj}];
+                end
+            end
+            rmpath(paths_excl_str{kk});
+            kk=kk+1;
+            
+        end
+        
+    end
+
+    
+    % Change the order of precedence of all the conflicting workspaces to
+    % the current one as primary workspace
+    if ~isempty(wsidx)
+        strtmp = wspaths{1};
+        celltmp = paths_excl_str{1};
+        
+        wspaths{1} = wspaths{wsidx};
+        paths_excl_str{1} = paths_excl_str{wsidx};
+        
+        wspaths{wsidx} = strtmp;
+        paths_excl_str{wsidx} = celltmp;        
     end
     
-    paths_excl_str = '';
-    for ii=1:length(paths_excl)
-        fprintf('Removing excluded path %s\n', paths_excl{ii});
-        if isempty(paths_excl_str)
-            paths_excl_str = paths_excl{ii};
-        else
-            paths_excl_str = [paths_excl_str, ';', paths_excl{ii}];
-        end
-    end
-    rmpath(paths_excl_str);
 end
 
--- setpaths.m
+++ setpaths.m
@@ -1,4 +1,4 @@
-function setpaths(add)
+function setpaths(add, mode)
 
 %
 % USAGE: 
@@ -28,59 +28,65 @@ function setpaths(add)
 if ~exist('add','var') || isempty(add)
     add = true;
 end
+if ~exist('mode','var') || isempty(mode)
+    mode = 'errcheck';
+end
 
-paths = getpaths();
+if ~add
+    mode = 'noerrcheck';
+end
+
+[paths, wspaths, paths_excl_str] = getpaths(mode);
+if ~isempty(wspaths)
+    if pathscompare(wspaths{1}, pwd)
+        fprintf('Current workspace %s already at the top of the search path.\n', wspaths{1});
+        addwspaths(wspaths, paths_excl_str);
+        return;
+    end
+end
+
 
 % Add or remove paths for this application
 rootpath = pwd;
 rootpath(rootpath=='\')='/';
 paths_str = '';
-err = false;
 for ii=1:length(paths)
     paths{ii} = [rootpath, paths{ii}];
-    if add
-        fprintf('Adding path %s\n', paths{ii});
-    else
-        fprintf('Removing path %s\n', paths{ii});
+    
+    if ~isempty(strfind(mode, 'verbose'))
+        if add
+            fprintf('Adding path %s\n', paths{ii});
+        else
+            fprintf('Removing path %s\n', paths{ii});
+        end
     end
     
     if ~exist(paths{ii}, 'dir')
         fprintf('Path %s does not exist\n', paths{ii});
-        err = true;
-        continue;
+        menu('WARNING: The current folder does NOT look like the application root folder. Please change current folder to the root application folder and rerun setpaths.', 'OK');
+        return;
     end
-    if isempty(paths_str)
-        paths_str = paths{ii};
-    else
-        paths_str = [paths_str, ';', paths{ii}];
-    end
-end
-
-if err
-    menu('WARNING: The current folder does NOT look like the application root folder. Please change current folder to the root application folder and rerun setpaths.', 'OK');
-    paths = {};
-    return;
-end
-
-
-if add
-    fprintf('ADDED application paths to matlab search paths:\n');
-    addpath(paths_str, '-end')
     
-    if isunix()
-        idx = findExePaths(paths);
-        for ii=1:length(idx)
-            fprintf(sprintf('chmod 755 %s/*\n', paths{idx(ii)}));
-            files = dir([paths{idx(ii)}, '/*']);
-            if ~isempty(files)
-                system(sprintf('chmod 755 %s/*', paths{idx(ii)}));
-            end
-        end
-    end
-else
-    fprintf('REMOVED application paths from matlab search paths:\n');
-    rmpath(paths_str);
+    paths_str = [paths_str, delimiter, paths{ii}];
 end
+
+
+% Add current workspace at the top of the stack of conflicting workspaces
+wspaths = [pwd; wspaths];
+paths_excl_str = [paths_str, paths_excl_str];
+
+
+% Either add all conflicting workspaces to the search path or remove the
+% current one, depending on user selection 
+if add
+    fprintf('ADDED search paths for worspace %s\n', pwd);    
+    addwspaths(wspaths, paths_excl_str);
+    setpermissions(paths);
+else
+    fprintf('REMOVED search paths for worspace %s\n', pwd);
+    rmpath(paths_excl_str{1});
+end
+
 
 
 
@@ -90,9 +96,51 @@ function idx = findExePaths(paths)
 idx = [];
 kk = 1;
 for ii=1:length(paths)
-    if ~isempty(findstr(paths{ii}, '/bin'))
+    if ~isempty(strfind(paths{ii}, '/bin'))
         idx(kk) = ii;
         kk=kk+1;
     end
 end
+
+
+
+% ---------------------------------------------------
+function addwspaths(wspaths, paths_excl_str)
+
+if isempty(wspaths)
+    return;
+end
+
+addpath(paths_excl_str{1}, '-end');
+
+if length(wspaths)>1
+    fprintf('\n');
+    fprintf('Order of precedence for conflicting workspaces:\n');
+else
+    return;
+end
+for ii=1:length(wspaths)
+    fprintf('  %s\n', wspaths{ii});
+    addpath(paths_excl_str{ii}, '-end');
+end
+
+
+
+% ---------------------------------------------------
+function setpermissions(paths)
+
+if isunix()
+    idx = findExePaths(paths);
+    for ii=1:length(idx)
+        fprintf(sprintf('chmod 755 %s/*\n', paths{idx(ii)}));
+        files = dir([paths{idx(ii)}, '/*']);
+        if ~isempty(files)
+            system(sprintf('chmod 755 %s/*', paths{idx(ii)}));
+        end
+    end
+end
+
+
+
+
 

817c1e4e988c4d3cae933e9bbbc04a696d8583be
jdubb@bu.edu <jdubb@bu.edu@85125e4a-4e41-11e8-8dd2-6be9935dadc8>
2019-02-26 04:36:17 +0000

    v1.7.4
    -- Integrated run-level and subject-level processing of SNIRF user functions. Group level still uses nirs-style processing user function.

--- Utils/getVernum.m
+++ Utils/getVernum.m
@@ -1,5 +1,5 @@
 function vrnnum = getVernum()
 
 vrnnum{1} = '1.7';  % Major version number
-vrnnum{2} = '3';   % Minor version number
+vrnnum{2} = '4';   % Minor version number
 vrnnum{3} = '';     % Patch #: 'p1', 'p2', etc

314ee70c4b4876b3ec85db2505c548f5aa51f7ab
sstucker <sstucker@bu.edu>
2020-06-17 09:59:13 -0400

    Added handles for ALL gui elements to MainGUI_EnableDisableGUI

--- MainGUI/MainGUI.m
+++ MainGUI/MainGUI.m
@@ -93,6 +93,26 @@ set(handles.radiobuttonPlotOD,  'enable', val);
 set(handles.radiobuttonPlotConc, 'enable', val);
 set(handles.checkboxPlotHRF, 'enable', val);
 set(handles.textStatus, 'enable', val);
+set(handles.listboxPlotConc, 'enable', val);
+% Plot window panel
+set(handles.pushbuttonPanLeft, 'enable', val);
+set(handles.pushbuttonPanRight, 'enable', val);
+set(handles.pushbuttonPanLeft, 'enable', val);
+set(handles.pushbuttonResetView, 'enable', val);
+set(handles.pushbuttonPanLeft, 'enable', val);
+set(handles.checkboxFixRangeX, 'enable', val);
+set(handles.editFixRangeX, 'enable', val);
+set(handles.checkboxFixRangeY, 'enable', val);
+set(handles.editFixRangeY, 'enable', val);
+% Motion artifact panel
+set(handles.checkboxShowExcludedTimeManual, 'enable', val);
+set(handles.checkboxShowExcludedTimeAuto, 'enable', val);
+set(handles.checkboxShowExcludedTimeAutoByChannel, 'enable', val);
+set(handles.checkboxExcludeTime, 'enable', val);
+% Control
+set(handles.pushbuttonCalcProcStream, 'enable', val);
+set(handles.pushbuttonProcStreamOptionsEdit, 'enable', val);
+set(handles.checkboxApplyProcStreamEditToAll, 'enable', val);
 
 
 % --------------------------------------------------------------------

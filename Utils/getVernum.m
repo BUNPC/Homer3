e2d5acbdbd7e89868111cabbb5bb6454862b1fa3
sstucker <sstucker@bu.edu>
2020-06-18 18:08:28 -0400

    Version number for merge

--- Utils/getVernum.m
+++ Utils/getVernum.m
@@ -1,6 +1,6 @@
 function vrnnum = getVernum()
 
 vrnnum{1} = '1';   % Major version #
-vrnnum{2} = '20';  % Major sub-version #
+vrnnum{2} = '21';  % Major sub-version #
 vrnnum{3} = '3';   % Minor version #
 vrnnum{4} = '0';   % Minor sub-version # or patch #: 'p1', 'p2', etc

464207a1ace17538b00a917743842150eb22a792
sstucker <sstucker@bu.edu>
2020-06-18 17:10:30 -0400

    checkForHomerUpdates now uses builtin Browser only, and only checks once a week

--- Utils/checkForHomerUpdates.m
+++ Utils/checkForHomerUpdates.m
@@ -1,45 +1,65 @@
 function checkForHomerUpdates()
-    url = 'http://bu.edu/neurophotonics/research/fnirs/homer3';
+    
     cfg = ConfigFileClass();
-    promptFlag = 0;
-    % Open a hidden web browser to run Google Analytics code
-    try
-        % Open a hidden web browser 
-        [~,h] = web(url);
-        p = getParentRecursive(h);
-        p.setVisible(0);
-        s = urlread(url,'timeout',2)
-    catch
-        % App is offline or server could not be reached
-        fprintf("Could not reach server to check for updates.\n");
-        close(h);
-        
-        return
+    
+    % If it has been a week since Homer checked for an update
+    if (datetime - cfg.GetValue('Last Checked For Update') > duration(168,0,0))
+       
+        % If user has not refused this update
+       if (~strcmp(cfg.GetValue('Update Refused'), version2string))
+            
+            url = 'http://bu.edu/neurophotonics/research/fnirs/homer3';
+            promptFlag = 0;
+
+            [s,status] = urlread(url,'timeout',4);
+            if (~status)
+                % App is offline or server could not be reached
+                fprintf('Server could not be reached to check for updates.') 
+                return
+            end
+            
+            cfg.SetValue('Last Checked For Update', datetime);
+
+            % Open a hidden web browser 
+            wb = com.mathworks.mde.webbrowser.WebBrowser.createBrowser;
+            wb.setCurrentLocation(url);
+            p = getParentRecursive(wb);
+            p.setVisible(0);
+
+%             out = regexp(s, '<a id="version">(.*?)<\/a>', 'match')
+            vrnum = getVernum();  % Compare to current version and set promptFlag
+%             if (vrnum < updateTxt) & (cfg.GetValue('LatestUpdateRefused') < updateTxt)
+%               promptFlag = 1;
+%             end
+
+            if (promptFlag)
+                choice = questdlg(['An update for Homer3 is available: ',...
+                    updateTxt,...
+                    ' Would you like to download it?'],...
+                    'Update Available',...
+                    'Yes','Remind me later','Don''t show this again',...
+                    'Remind me later');
+                switch choice
+                    case 'Yes'
+                        % Open browser to update page
+                        close(wb);
+                        web(url);    
+                    case 'Don''t ask again'
+                        % Make sure user doesn't get asked about this particular
+                        % update again.
+                        cfg.SetValue('Update Refused', updateTxt);
+                        
+                end
+                
+            end
+            
+            pause(1);
+            close(wb);
+            cfg.WriteFile();
+            cfg.Close();
+            
+       end
+       
     end
     
-    updateTxt = ''; % Get information about potential update from s
-    vrnum = getVernum();  % Compare to current version and set promptFlag
-    % if (vrnum < updateTxt) & (cfg.GetValue('LatestUpdateRefused') < updateTxt)
-    %   promptFlag = 1;
-    % end
-    
-    if (promptFlag)
-        choice = questdlg(['An update for Homer3 is available: ',...
-            updateTxt,...
-            ' Would you like to download it?'],...
-            'Update Available',...
-            'Yes','Remind me later','Don''t show this again',...
-            'Remind me later');
-        switch choice
-            case 'Yes'
-                % Open browser to update page
-                web(url);    
-            case 'Don''t ask again'
-                % Make sure user doesn't get asked about this particular
-                % update again.
-                %
-                % cfg.SetValue('LatestUpdateRefused',updateTxt); 
-        end
-    end
-    close(h);
 end

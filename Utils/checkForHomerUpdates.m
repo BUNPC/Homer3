87f8961ad3aa4f6f95f8a60351ee93e879a350b8
sstucker <sstucker@bu.edu>
2020-06-23 12:34:12 -0400

    1.21.5
    --Added settings dropdown menu  --Checking for updates can be disabled from settings dropdown menu

--- Utils/checkForHomerUpdates.m
+++ Utils/checkForHomerUpdates.m
@@ -2,59 +2,65 @@ function checkForHomerUpdates()
     
     cfg = ConfigFileClass();
     
-    % If it has been a week since Homer checked for an update
-    if (datetime - cfg.GetValue('Last Checked For Update') > duration(168,0,0))
+    % If user has Check For Updates enabled
+    if (strcmp(cfg.GetValue('Check For Updates'),'on'))
 
-        url = 'http://bu.edu/neurophotonics/research/fnirs/homer3';
-        promptFlag = 0;
+        % If it has been a week since Homer checked for an update
+        if (datetime - cfg.GetValue('Last Checked For Update') > duration(168,0,0))
 
-        [s,status] = urlread(url,'timeout',4);
-        if (~status)
-            % App is offline or server could not be reached
-            fprintf('Server could not be reached to check for updates.') 
-            return
-        end
+            url = 'http://bu.edu/neurophotonics/research/fnirs/homer3';
+            promptFlag = 0;
 
-        cfg.SetValue('Last Checked For Update', datetime);
+            [s,status] = urlread(url,'timeout',4);
+            if (~status)
+                % App is offline or server could not be reached
+                fprintf('Server could not be reached to check for updates.') 
+                return
+            end
 
-        % Open a hidden web browser 
-        wb = com.mathworks.mde.webbrowser.WebBrowser.createBrowser;
-        wb.setCurrentLocation(url);
-        p = getParentRecursive(wb);
-        p.setVisible(0);
+            cfg.SetValue('Last Checked For Update', datetime);
+
+            % Open a hidden web browser 
+            wb = com.mathworks.mde.webbrowser.WebBrowser.createBrowser;
+            wb.setCurrentLocation(url);
+            p = getParentRecursive(wb);
+            p.setVisible(0);
 
 %             out = regexp(s, '<a id="version">(.*?)<\/a>', 'match')
-        vrnum = getVernum();  % Compare to current version and set promptFlag
+            vrnum = getVernum();  % Compare to current version and set promptFlag
 %             if (vrnum < updateTxt) & (cfg.GetValue('LatestUpdateRefused') < updateTxt)
 %               promptFlag = 1;
 %             end
 
-        if (promptFlag)
-            choice = questdlg(['An update for Homer3 is available: ',...
-                updateTxt,...
-                ' Would you like to download it?'],...
-                'Update Available',...
-                'Yes','Remind me later','Don''t show this again',...
-                'Remind me later');
-            switch choice
-                case 'Yes'
-                    % Open browser to update page
-                    close(wb);
-                    web(url);    
-                case 'Don''t ask again'
-                    % Make sure user doesn't get asked about this particular
-                    % update again.
-                    cfg.SetValue('Update Refused', updateTxt);
+            if (promptFlag)
+                choice = questdlg(['An update for Homer3 is available: ',...
+                    updateTxt,...
+                    ' Would you like to download it?'],...
+                    'Update Available',...
+                    'Yes','Remind me later','Don''t show this again',...
+                    'Remind me later');
+                switch choice
+                    case 'Yes'
+                        % Open browser to update page
+                        close(wb);
+                        web(url);    
+                    case 'Don''t ask again'
+                        % Make sure user doesn't get asked about this particular
+                        % update again.
+                        cfg.SetValue('Update Refused', updateTxt);
+
+                end
 
             end
 
-        end
+            pause(1);  % To ensure <script> is run
+            close(wb);
+            cfg.Save();
 
-        pause(1);
-        close(wb);
-        cfg.Save();
-        cfg.Close();
-       
+        end
+    
     end
+    
+    cfg.Close();
     
 end

2941bed680e5e28b1fc626cab029bf78a2afd4d2
sstucker <sstucker@bu.edu>
2020-07-15 16:22:21 -0400

    Removed reference to global variable from EnableDisable method

--- MainGUI/MainGUI.m
+++ MainGUI/MainGUI.m
@@ -88,42 +88,42 @@ uipanelProcessingType_SelectionChangeFcn([]);
 % ---------------------------------------------------------------------
 function MainGUI_EnableDisableGUI(handles, val)
 
-    set(maingui.handles.listboxGroupTree, 'enable', val);
-    set(maingui.handles.radiobuttonProcTypeGroup, 'enable', val);
-    set(maingui.handles.radiobuttonProcTypeSubj, 'enable', val);
-    set(maingui.handles.radiobuttonProcTypeRun, 'enable', val);
-    set(maingui.handles.radiobuttonPlotRaw, 'enable', val);
-    set(maingui.handles.radiobuttonPlotOD,  'enable', val);
-    set(maingui.handles.radiobuttonPlotConc, 'enable', val);
-    set(maingui.handles.checkboxPlotHRF, 'enable', val);
-    set(maingui.handles.textStatus, 'enable', val);
-    set(maingui.handles.listboxPlotConc, 'enable', val);
+    set(handles.listboxGroupTree, 'enable', val);
+    set(handles.radiobuttonProcTypeGroup, 'enable', val);
+    set(handles.radiobuttonProcTypeSubj, 'enable', val);
+    set(handles.radiobuttonProcTypeRun, 'enable', val);
+    set(handles.radiobuttonPlotRaw, 'enable', val);
+    set(handles.radiobuttonPlotOD,  'enable', val);
+    set(handles.radiobuttonPlotConc, 'enable', val);
+    set(handles.checkboxPlotHRF, 'enable', val);
+    set(handles.textStatus, 'enable', val);
+    set(handles.listboxPlotConc, 'enable', val);
     % Plot window panel
-    set(maingui.handles.pushbuttonPanLeft, 'enable', val);
-    set(maingui.handles.pushbuttonPanRight, 'enable', val);
-    set(maingui.handles.pushbuttonPanLeft, 'enable', val);
-    set(maingui.handles.pushbuttonResetView, 'enable', val);
-    set(maingui.handles.pushbuttonPanLeft, 'enable', val);
-    set(maingui.handles.checkboxFixRangeX, 'enable', val);
-    set(maingui.handles.editFixRangeX, 'enable', val);
-    set(maingui.handles.checkboxFixRangeY, 'enable', val);
-    set(maingui.handles.editFixRangeY, 'enable', val);
+    set(handles.pushbuttonPanLeft, 'enable', val);
+    set(handles.pushbuttonPanRight, 'enable', val);
+    set(handles.pushbuttonPanLeft, 'enable', val);
+    set(handles.pushbuttonResetView, 'enable', val);
+    set(handles.pushbuttonPanLeft, 'enable', val);
+    set(handles.checkboxFixRangeX, 'enable', val);
+    set(handles.editFixRangeX, 'enable', val);
+    set(handles.checkboxFixRangeY, 'enable', val);
+    set(handles.editFixRangeY, 'enable', val);
     % Motion artifact panel
-    set(maingui.handles.checkboxShowExcludedTimeManual, 'enable', val);
-    set(maingui.handles.checkboxShowExcludedTimeAuto, 'enable', val);
-    set(maingui.handles.checkboxShowExcludedTimeAutoByChannel, 'enable', val);
-    set(maingui.handles.checkboxExcludeTime, 'enable', val);
+    set(handles.checkboxShowExcludedTimeManual, 'enable', val);
+    set(handles.checkboxShowExcludedTimeAuto, 'enable', val);
+    set(handles.checkboxShowExcludedTimeAutoByChannel, 'enable', val);
+    set(handles.checkboxExcludeTime, 'enable', val);
     % Control
-    set(maingui.handles.pushbuttonCalcProcStream, 'enable', val);
-    set(maingui.handles.pushbuttonProcStreamOptionsEdit, 'enable', val);
-    set(maingui.handles.checkboxApplyProcStreamEditToAll, 'enable', val);
+    set(handles.pushbuttonCalcProcStream, 'enable', val);
+    set(handles.pushbuttonProcStreamOptionsEdit, 'enable', val);
+    set(handles.checkboxApplyProcStreamEditToAll, 'enable', val);
     % Menu
-    set(maingui.handles.ToolsMenu, 'enable', val);
-    set(maingui.handles.ViewMenu, 'enable', val);
-    set(maingui.handles.menuItemSaveGroup, 'enable', val);
-    set(maingui.handles.menuItemExport, 'enable', val);
-    set(maingui.handles.menuItemReset, 'enable', val);
-    set(maingui.handles.menuItemResetGroupFolder, 'enable', val)
+    set(handles.ToolsMenu, 'enable', val);
+    set(handles.ViewMenu, 'enable', val);
+    set(handles.menuItemSaveGroup, 'enable', val);
+    set(handles.menuItemExport, 'enable', val);
+    set(handles.menuItemReset, 'enable', val);
+    set(handles.menuItemResetGroupFolder, 'enable', val)
 
 
 % --------------------------------------------------------------------

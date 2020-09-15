762aa1cc858412b162eb3fbfecbb8e00b29ab571
sstucker <sstucker@bu.edu>
2020-09-15 01:33:28 -0400

    Update vernum to 1.26.0

--- Utils/getVernum.m
+++ Utils/getVernum.m
@@ -1,6 +1,6 @@
 function vrnnum = getVernum()
 
 vrnnum{1} = '1';   % Major version #
-vrnnum{2} = '25';  % Major sub-version #
-vrnnum{3} = '2';   % Minor version #
+vrnnum{2} = '26';  % Major sub-version #
+vrnnum{3} = '0';   % Minor version #
 vrnnum{4} = '0';   % Minor sub-version # or patch #: 'p1', 'p2', etc

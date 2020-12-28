9e9a054bbf1da7c88b926418829a4425b6a2e1f4
sstucker <sstucker@bu.edu>
2020-12-28 12:38:54 -0500

    Version number

--- Utils/getVernum.m
+++ Utils/getVernum.m
@@ -1,6 +1,6 @@
 function vrnnum = getVernum()
 
 vrnnum{1} = '1';   % Major version #
-vrnnum{2} = '28';  % Major sub-version #
-vrnnum{3} = '7';   % Minor version #
+vrnnum{2} = '29';  % Major sub-version #
+vrnnum{3} = '0';   % Minor version #
 vrnnum{4} = '0';   % Minor sub-version # or patch #: 'p1', 'p2', etc

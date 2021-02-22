4af3ed50615f6ecd8b2e8789614ea84fef4ed6a7
sstucker <sstucker@bu.edu>
2021-02-22 17:48:07 -0500

    Updated hmrR_errchk with c_vector param

--- FuncRegistry/UserFunctions/hmrR_GLM_errchk.m
+++ FuncRegistry/UserFunctions/hmrR_GLM_errchk.m
@@ -1,12 +1,13 @@
 % PARAMETERS:
 % trange: [-2.0, 20.0]
 % glmSolveMethod: 1
-% idxBasis: 2
-% paramsBasis: [0.1 3.0 10.0 1.8 3.0 10.0], maxnum: 6
+% idxBasis: 1
+% paramsBasis: [1.0 1.0 0.0 0.0 0.0 0.0], maxnum: 6
 % rhoSD_ssThresh: 15.0
 % flagNuisanceRMethod: 1
 % driftOrder: 3
-function errmsg = hmrR_GLM_errchk(trange, glmSolveMethod, idxBasis, paramsBasis, rhoSD_ssThresh, flagNuisanceRMethod, driftOrder)
+% c_vector: 0
+function errmsg = hmrR_GLM_errchk(trange, glmSolveMethod, idxBasis, paramsBasis, rhoSD_ssThresh, flagNuisanceRMethod, driftOrder, c_vector)
     errmsg = '';
     % Define parameter error cases below and return an informative message
     if idxBasis > 4 || idxBasis < 1

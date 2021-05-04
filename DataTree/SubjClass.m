eacbea8ebdd012a419b2ab5bc95eceaf106af3ab
Meryem Ayse Yucel <49535526+mayucel@users.noreply.github.com>
2021-05-04 06:28:13 -0400

    Update SubjClass.m
    Extracting right stim from run level output. Merely GetVars for stim does not work as it does not include the stim changes with stim reject.

--- DataTree/SubjClass.m
+++ DataTree/SubjClass.m
@@ -248,7 +248,15 @@ classdef SubjClass < TreeNodeClass
             obj.outputVars.tHRFRuns{r.iRun}      = r.procStream.output.GetTHRF();
             obj.outputVars.mlActRuns{r.iRun}     = r.procStream.output.GetVar('mlActAuto');
             obj.outputVars.nTrialsRuns{r.iRun}   = r.procStream.output.GetVar('nTrials');
-            obj.outputVars.stimRuns{r.iRun}      = r.procStream.output.misc.stim;
+            if ~isempty(r.procStream.output.GetVar('misc'))
+                if isfield(r.procStream.output.misc, 'stim') == 1
+                    obj.outputVars.stimRuns{r.iRun}      = r.procStream.output.misc.stim;
+                else
+                    obj.outputVars.stimRuns{r.iRun}      = r.GetVar('stim');
+                end
+            else
+                obj.outputVars.stimRuns{r.iRun}      = r.GetVar('stim');
+            end
             obj.outputVars.dcRuns{r.iRun}       = r.procStream.output.GetVar('dc');
             obj.outputVars.AauxRuns{r.iRun}      = r.procStream.output.GetVar('Aaux');
             obj.outputVars.tIncAutoRuns{r.iRun}  = r.procStream.output.GetVar('tIncAuto');

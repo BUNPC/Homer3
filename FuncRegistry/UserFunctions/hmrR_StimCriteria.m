03d1e09bbed0c2c56cffefec6a83ddb123b97a16
sstucker <sstucker@bu.edu>
2021-07-02 19:09:47 -0400

    hmrR_StimCriteria takes string as param

--- FuncRegistry/UserFunctions/hmrR_StimCriteria.m
+++ FuncRegistry/UserFunctions/hmrR_StimCriteria.m
@@ -1,5 +1,5 @@
 % SYNTAX:
-% stim = hmrR_StimCriteria(stim, stimDataColIdx, logic, threshold)
+% stim = hmrR_StimCriteria(stim, stimDataLabel, logic, threshold)
 %
 % UI NAME:
 % Stim_Apply_Criteria
@@ -8,39 +8,42 @@
 % Excludes stims based on the scalar values associated with them. A stim
 % mark's onset, duration, or amplitude can be used. Additional scalar values
 % defined in optional additional columns of the SNIRF stim data matrix can
-% be used.
+% also be used.
 %
 % INPUT: 
 % stim:     SNIRF stim object
-% stimDataColIdx: The stim data value to use as the criterion. Equivalent to
-%           a column index into the SNIRF stim data matrix, i.e.
-%           1. The stim onset time
-%           2. The stim duration
-%           3. The stim amplitude
-%           Values of 4 and above select any additional columns of the 
-%           SNIRF stim data matrix which may contain scalar values associated
-%           with each stim mark.
+% stimDataLabel: The label of the column to use as the criterion. i.e.
+%           Onset, Amplitude, Duration, etc. Not case-sensitive. If a run
+%           does not have a stim data for the given column, stims in this
+%           run are not altered.
 % logic:    If 1, only stims which fall within the threshold range are enabled. 
 %           If 0, only stims which outside the threshold range are enabled.
 % threshold: The scalar values which bound the values of the selected stim
-%           criterion.
+%            criterion.
 %
 % OUTPUT:
 % stim:     SNIRF stim object
 %
 % USAGE OPTIONS:
-% Stim_Apply_Criteria: stim = hmrR_StimCriteria(stim, stimDataColIdx, logic, threshold)
+% Stim_Apply_Criteria: stim = hmrR_StimCriteria(stim, stimDataLabel, logic, threshold)
 %
 % PARAMETERS:
-% stimDataColIdx: 1
+% stimDataLabel: Amplitude
 % logic: 1
 % threshold: [-1.00, 1.00]
 %
-function stim = hmrR_StimCriteria(stim, stimDataColIdx, logic, threshold)
+function stim = hmrR_StimCriteria(stim, stimDataLabel, logic, threshold)
 
 % Exit if there are no stims
 if isempty(stim)
     return;
+end
+
+for i = 1:length(stim)
+    stimDataColIdx = find(strcmp(lower(stim(i).dataLabels), lower(stimDataLabel)));
+    if isempty(stimDataColIdx)
+        return;
+    end
 end
 
 % Exit if stimDataColIdx is too large for the stim data array
@@ -53,9 +56,9 @@ end
 for i = 1:length(stim)
     if ~isempty(stim(i).data)
         stim(i).states(:, 2) = -2;  % Automatically disable all stims
-        for j = 1:size(stim(i), 1)
+        for j = 1:size(stim(i).data, 1)
             % If stim criterion is within threshold
-            if stim(i).data(j, stimDataColIdx) > threshold(1) & stim(i).data(j, stimDataColIdx) < threshold(2)
+            if (stim(i).data(j, stimDataColIdx) > threshold(1)) && (stim(i).data(j, stimDataColIdx) < threshold(2))
                 if logic
                     stim(i).states(j, 2) = 1;
                 end

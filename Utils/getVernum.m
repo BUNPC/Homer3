97f334dc8ee50a8b93d3ad40265338f980fa1700
jayd1860 <jayd1860@gmail.com>
2021-08-11 19:01:25 -0500

    v1.32.4
    -- Copy versoin number for Homer3 from AV

--- Utils/getVernum.m
+++ Utils/getVernum.m
@@ -20,7 +20,7 @@ function [vrnnum] = getVernum_AtlasViewerGUI()
 
 vrnnum{1} = '2';   % Major version #
 vrnnum{2} = '15';  % Major sub-version #
-vrnnum{3} = '4';   % Minor version #
+vrnnum{3} = '5';   % Minor version #
 vrnnum{4} = '0';   % Minor sub-version # or patch #: 'p1', 'p2', etc
 
 

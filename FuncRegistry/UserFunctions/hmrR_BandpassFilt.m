679739c3ae75f2d373172120da8f15c6e1efe1fd
Sofía Sappia <sofia@artinis.com>
2021-10-27 11:59:43 +0200

    Adjusts to argument validation syntax

--- FuncRegistry/UserFunctions/hmrR_BandpassFilt.m
+++ FuncRegistry/UserFunctions/hmrR_BandpassFilt.m
@@ -29,6 +29,16 @@
 % Intensity_to_Delta_OD: dod = hmrR_Intensity2OD( intensity )
 
 function [data2, ylpf] = hmrR_BandpassFilt( data, hpf, lpf )
+
+arguments
+    data {mustBeNonNanData(data), mustBeFiniteData(data)}
+    hpf double {mustBeNumeric, mustBeReal}
+    lpf double {mustBeNumeric, mustBeReal, mustNotExceedNyquist(data, hpf, lpf)}
+
+end
+
+
+
 if isa(data, 'DataClass')
     data2 = DataClass().empty();
 elseif isa(data, 'AuxClass')
@@ -44,39 +54,22 @@ for ii=1:length(data)
     y = data2(ii).GetDataTimeSeries();
     y2 = y;
     fs = data2(ii).GetTime();
-    
+
     % convert t to fs
     % assume fs is a time vector if length>1
     if length(fs)>1
         fs = 1/(fs(2)-fs(1));
     end
-    
-    % Check for NaN
-    if max(max(isnan(y)))
-       warning('Input to hmrR_BandpassFilt contains NaN values. Add hmrR_PreprocessIntensity_NAN to the processing stream.');
-       return
-    end
-    % Check for finite values
-    if max(max(isinf(y)))
-       warning('Input to hmrR_BandpassFilt must be finite.');
-       return
-    end
 
-    % Check that cutoff < nyquist
-    if lpf / (fs / 2) > 1 || hpf / (fs / 2) > 1
-        warning(['hmrR_BandpassFilt cutoff cannot exceed the folding frequency of the data with sample rate ', num2str(fs), ' hz.']);
-        return
-    end
-    
     % low pass filter
     lpf_norm = lpf / (fs / 2);
-    if lpf_norm > 0  % No lowpass if filter is 
+    if lpf_norm > 0  % No lowpass if filter is
         FilterOrder = 3;
         [z, p, k] = butter(FilterOrder, lpf_norm, 'low');
         [sos, g] = zp2sos(z, p, k);
-        y2 = filtfilt(sos, g, double(y)); 
+        y2 = filtfilt(sos, g, double(y));
     end
-    
+
     % high pass filter
     hpf_norm = hpf / (fs / 2);
     if hpf_norm > 0
@@ -85,7 +78,68 @@ for ii=1:length(data)
         [sos, g] = zp2sos(z, p, k);
         y2 = filtfilt(sos, g, y2);
     end
-    
+
     data2(ii).SetDataTimeSeries(y2);
-    
+
 end
+end
+
+%--------------------------------------------------------------------------
+% validation functions
+function mustBeNonNanData(data)
+for ii = 1:length(data)
+    if isa(data, 'DataClass')
+        data2 = DataClass(data(ii));
+    elseif isa(data, 'AuxClass')
+        data2 = AuxClass(data(ii));
+    end
+    y = data2.GetDataTimeSeries();
+    if max(max(isnan(y)))
+        throwAsCaller(MException('Homer:ContainsNan', ...
+            ['Input to hmrR_BandpassFilt contains NaN values. ' ...
+            'Add hmrR_PreprocessIntensity_NAN to the processing stream.']));
+    end
+end
+end
+
+function mustBeFiniteData(data)
+for ii = 1:length(data)
+    if isa(data, 'DataClass')
+        data2 = DataClass(data(ii));
+    elseif isa(data, 'AuxClass')
+        data2 = AuxClass(data(ii));
+    end
+    y = data2.GetDataTimeSeries();
+    if max(max(isinf(y)))
+        throwAsCaller(MException('Homer:NonFiniteInput', ...
+            'Input to hmrR_BandpassFilt must be finite.'));
+
+    end
+end
+end
+
+function mustNotExceedNyquist(data, hpf, lpf)
+for ii = 1:length(data)
+    if isa(data, 'DataClass')
+        data2 = DataClass(data(ii));
+    elseif isa(data, 'AuxClass')
+        data2 = AuxClass(data(ii));
+    end
+    fs = data2.GetTime();
+    % convert t to fs
+    % assume fs is a time vector if length>1
+    if length(fs)>1
+        fs = 1/(fs(2)-fs(1));
+    end
+    % Check that cutoff < nyquist
+    if lpf / (fs / 2) > 1 || hpf / (fs / 2) > 1
+        throwAsCaller(MException('Homer:ExceedsNyquist', ...
+            ['hmrR_BandpassFilt cutoff cannot exceed the folding ' ...
+            'frequency of the data with sample rate '], ...
+            num2str(fs), ' hz.'))
+    end
+
+end
+end
+
+

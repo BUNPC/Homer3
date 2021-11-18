70a0da0af5b22aa46c957e543ecd03c07c94c8c8
Sofía Sappia <sofia@artinis.com>
2021-11-18 11:35:42 +0100

    Adds argument validation error tests

---
+++ UnitTests/unitTest_OD2Conc_ArgumentValidation.m
@@ -0,0 +1,43 @@
+% This script checks for expected errors given the argument validation
+% functions
+
+% load test data file
+fname = 'Example_RunFuncOffline/test.snirf';
+data_nirs = {SnirfClass(fname)}; 
+acquired = data_nirs{1};
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Error checks
+%--------------------------------------------------------------------------
+testCase = matlab.unittest.TestCase.forInteractiveUse;
+
+% wrong object trhows class exception
+verifyError(testCase,@() hmrR_OD2Conc(acquired, acquired.probe, [1.0 1.0]), ...
+    "Class:notCorrectClass")
+verifyError(testCase,@() hmrR_OD2Conc(acquired.data, acquired, [1.0 1.0]), ...
+    "Class:notCorrectClass")
+
+% check ppf mustBeNumeric
+verifyError(testCase,@() hmrR_OD2Conc(acquired.data, acquired.probe, ['h' 1.0]), ...
+    "MATLAB:validators:mustBeNumeric")
+verifyError(testCase,@() hmrR_OD2Conc(acquired.data, acquired.probe, [true true]), ...
+    "MATLAB:validators:mustBeNumeric")
+verifyError(testCase,@() hmrR_OD2Conc(acquired.data, acquired.probe, [1 'h']), ...
+    "MATLAB:validators:mustBeNumeric")
+
+% check ppf mustBePositive
+verifyError(testCase,@() hmrR_OD2Conc(acquired.data, acquired.probe, [-1.0 -1.0]), ...
+    "MATLAB:validators:mustBePositive")
+verifyError(testCase,@() hmrR_OD2Conc(acquired.data, acquired.probe, [1.0 -1.0]), ...
+    "MATLAB:validators:mustBePositive")
+verifyError(testCase,@() hmrR_OD2Conc(acquired.data, acquired.probe, [-1.0 1.0]), ...
+    "MATLAB:validators:mustBePositive")
+verifyError(testCase,@() hmrR_OD2Conc(acquired.data, acquired.probe, [0 1.0]), ...
+    "MATLAB:validators:mustBePositive")
+verifyError(testCase,@() hmrR_OD2Conc(acquired.data, acquired.probe, [1.0 0]), ...
+    "MATLAB:validators:mustBePositive")
+verifyError(testCase,@() hmrR_OD2Conc(acquired.data, acquired.probe, [0 0]), ...
+    "MATLAB:validators:mustBePositive")
+
+
+

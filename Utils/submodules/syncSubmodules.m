ddb0f7d2b9f3019e4fa8d78e58c269b6cbea3616
jayd1860 <jdubb@bu.edu>
2022-02-03 11:49:04 -0500

    -- Change syncSubmodules.m to compare versions and to make the source and destination libraries based on which version is higher.

--- Utils/submodules/syncSubmodules.m
+++ Utils/submodules/syncSubmodules.m
@@ -1,13 +1,33 @@
 function [cmds, errs, msgs] = syncSubmodules(repo, options, preview)
 
 % Syntax:
+%
 %   [cmds, errs, msgs] = syncSubmodules()
 %   [cmds, errs, msgs] = syncSubmodules(repo)
 %   [cmds, errs, msgs] = syncSubmodules(repo, options)
 %   [cmds, errs, msgs] = syncSubmodules(repo, options, preview)
 %
+% Inputs:
+%
+%   repo:       String type. Location of parent repository. This is the repo that has the built-in
+%               copy of the libraries. Default if argumenty not supplied is
+%               the current folder
+%
+%   options:    String argument with the possible values: {'init' | 'update'}
+%               'init'   - If standalone submodules have already been downloaded then do NOT
+%                          download again. Then sync the two libraries.
+%               'update' - If standalone submodules have already been downloaded then move 
+%                          the current version to submodules.old and download again. Then 
+%                          sync the two libraries
+%               Default if argument not supplied is 'init'
+%
+%   preview:    The parent repository. This is the repo that has the built-in
+%               copy of the libraries
+%
 % Examples:
-%   syncSubmodules(pwd, 'init', true);
+%
+%   syncSubmodules();
+%   syncSubmodules(pwd, 'init');
 %   syncSubmodules(pwd, 'update');
 %   syncSubmodules(pwd);
 %
@@ -19,7 +39,7 @@ if ~exist('repo','var') || isempty(repo)
     repo = [pwd, '/'];
 end
 if ~exist('options','var')
-    options = '';
+    options = 'init';
 end
 if ~exist('preview','var')
     preview = false;
@@ -34,30 +54,21 @@ if ~ispathvalid([repoFull, '/submodules']) || strcmp(options, 'update')
     [cmds, errs, msgs] = gitSubmodulesClone(repoFull);
 end
 
+fprintf('\n');
+
 cmds{ii,1} = sprintf('cd %s', repoFull); ii = ii+1;
 for jj = 1:size(submodules,1)
-    [~, f, e] = fileparts(submodules{jj,1});
-    submodulename = [f, e];
-    
-    repo1 = submodules{jj,2};
-    repo2 = [repoFull, 'submodules/', submodulename];
-    
-    if repo1(end)=='\' || repo1(end)=='/'
-        repo1 = repo1(1:end-1);
-    end
-    if repo2(end)=='\' || repo2(end)=='/'
-        repo2 = repo2(1:end-1);
-    end
+    [repo1, repo2, submodulename] = getRepos(submodules, jj, repoFull);
     
     fprintf('Synching "%s" library:\n', submodulename);
-    fprintf('Repo1:  %s, v%s\n', repo1, getVernum(submodulename, repo1));
-    fprintf('Repo2:  %s, v%s\n', repo2, getVernum(submodulename, repo2));
-    fprintf('====================================================================\n');   
+    fprintf('Repo1:  %s, v%s\n', repo1, getVernum(submodulename, repo1(1:end-1)));
+    fprintf('Repo2:  %s, v%s\n', repo2, getVernum(submodulename, repo2(1:end-1)));
+    fprintf('====================================================================\n');
     
     identical(1) = sync(repo1, repo2, false, preview);
     identical(2) = sync(repo2, repo1, true, preview);
-
-    if all(identical) 
+    
+    if all(identical)
         fprintf('Repos are IDENTICAL\n');
     end
     
@@ -91,7 +102,7 @@ for kk = 1:length(f2)
     [~, fname2,ext2] = fileparts(f2{kk});
     for ll = 1:length(f1)
         [~, fname1,ext1] = fileparts(f1{ll});
-        if strcmp([fname1,ext1], [fname2,ext2]) 
+        if strcmp([fname1,ext1], [fname2,ext2])
             if ~filesEqual(f2{kk}, f1{ll},'exact') && ~peerlessOnly
                 fprintf('Copying %s to %s\n', f2{kk}, f1{ll})
                 identical = false;
@@ -124,4 +135,63 @@ for kk = 1:length(f2)
     
 end
 
+
+
+
+% ----------------------------------------------------------------------------------
+function b = compareVersions(verstr1, verstr2)
+b = 1;
+
+v1 = str2cell(verstr1, '.');
+v2 = str2cell(verstr2, '.');
+
+% Now that we have the version numbers of both objects, we can
+% do an actual numeric comparison
+b = 0;
+for ii=1:max(length(v1), length(v2))
+    if ii>length(v1)
+        b = -1;
+        break;
+    end
+    if ii>length(v2)
+        b = 1;
+        break;
+    end
+    if v1{ii}>v2{ii}
+        b = 1;
+        break;
+    end
+    if v1{ii}<v2{ii}
+        b = -1;
+        break;
+    end
+end
+
+
+
+% -----------------------------------------------------------------------------------
+function [repo1, repo2, submodulename] = getRepos(submodules, jj, repoFull)
+[~, f, e] = fileparts(submodules{jj,1});
+submodulename = [f, e];
+
+r1 = submodules{jj,2};
+r2 = [repoFull, 'submodules/', submodulename];
+
+if r1(end)=='\' || r1(end)=='/'
+    r1 = r1(1:end-1);
+end
+if r2(end)=='\' || r2(end)=='/'
+    r2 = r2(1:end-1);
+end
+
+verstr1 = getVernum(submodulename, r1);
+verstr2 = getVernum(submodulename, r2);
+
+if compareVersions(verstr1, verstr2) < 1
+    repo1 = [r1, '/'];
+    repo2 = [r2, '/'];
+else
+    repo1 = [r2, '/'];
+    repo2 = [r1, '/'];
+end
 

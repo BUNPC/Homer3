cb8a3fbc444f0c5c5f826783049b919c1c164a17
jayd1860 <jdubb@bu.edu>
2022-03-14 14:49:54 -0400

    v1.35.10
    -- Fix error loading SNIRF aux.name field in AuxClass because of changes to MATLAB's HDF5 library. (It seems for string there is no need to transpose the data.)

--- DataTree/AcquiredData/DataFiles/Hdf5/HDF5_Transpose.m
+++ DataTree/AcquiredData/DataFiles/Hdf5/HDF5_Transpose.m
@@ -9,8 +9,8 @@ end
 % with the file format's storage order.
 if ~isrow(val) && ~iscolumn(val)            % Matrices
     val = permute(val, ndims(val):-1:1);
-elseif ischar(val)                          % 1D char strings 
-    val = permute(val, ndims(val):-1:1);
+% elseif ischar(val)                          % 1D char strings 
+%     val = permute(val, ndims(val):-1:1);
 elseif ~isempty(findstr('multidim', options))   % Force multi-dimensional even if vector
     val = permute(val, ndims(val):-1:1);
 elseif ~isempty(findstr('2D', options))         % Force 2D even if vector
